5bc37031808726964997c504f083b3c57a02ba79
Endolite <41879690+Endolite@users.noreply.github.com>
2022-05-21 00:41:37 -0400

    05/20/22 0041

--- Assignments/Project 1/Project 1 - Arnav Patri.cpp
+++ Assignments/Project 1/Project 1 - Arnav Patri.cpp
@@ -11,45 +11,64 @@ int main(){
   string players[2][3];
     players[0][0] = "one";
     players[1][0] = "two";
-    for (int i = 0; i < 2; i++){
+    /*for (int i = 0; i < 2; i++){
       players[i][1] = cards[rand() % 13];
       players[i][2] = suites[rand() % 4];
     }
-  for (int i = 0; i < 2; i++){
-    cout << "Player " + players[i][0] + " card is " + players[i][1] + " " +  players[i][2] + "!" << endl;
-  }
-  string c1 = "Player One Wins!";
-  string c2 = "Player Two Wins!";
-  string c3 = "It's a Draw!";
-  if (players[0][2] == players[1][2]){
-    if (players[0][1] == players[1][1]){
-      cout << c3;
+    while ((players[0][1] == players[1][1]) && (players[0][2] == players[1][2])){
+      players[1][1] = cards[rand() % 13];
+      players[1][2] = suites[rand() % 4];
+    }*/
+    int cases[4][2][2] = {
+      {
+        {2, 1}, {7, 0}
+      },
+      {
+        {12, 0}, {2, 1}
+      },
+      {
+        {2, 2}, {9, 3}
+      },
+      {
+        {9, 1}, {10, 3}
+      }
+    };
+  for (int a = 0; a < 4; a++){
+    for (int b = 0; b < 2; b++){
+      players[b][1] = cards[cases[a][b][0]];
+      players[b][2] = suites[cases[a][b][1]];
     }
-    else if (stoi(players[0][1]) > stoi(players[1][1])){
-      cout << c1;
+    for (int i = 0; i < 2; i++){
+      cout << "Player " + players[i][0] + " card is " + players[i][1] + " " +  players[i][2] + "!" << endl;
     }
-    else{
+    string c1 = "Player One Wins!";
+    string c2 = "Player Two Wins!";
+    string c3 = "It's a Draw!";
+    if (players[0][2] == "Hearts"){
+      if (players[1][2] == "Hearts"){
+        if (stoi(players[0][1]) < stoi(players[1][1])){
+          cout << c2;
+        }
+        else{
+          cout << c1;
+        }
+      }
+      else{
+        cout << c1;
+      }
+    }
+    else if (players[1][2] == "Hearts"){
       cout << c2;
     }
-  }
-  else if (players[0][0] == "Hearts"){
-    cout << c1;
-  }
-  else if (players[0][0] == "Hearts"){
-    cout << c2;
-  }
-  else if (stoi(players[0][1]) < stoi(players[1][1])){
-    cout << c2;
-  }
-  else{
-    cout << c1;
-  }
-  if (players[0][2] == "Hearts"){
-    if (players[2][2] == "Hearts"){
-      
+    else if (stoi(players[0][1]) == stoi(players[1][1])){
+      cout << c3;
     }
-    else cout << c1;
+    else if (stoi(players[0][1]) < stoi(players[1][1])){
+      cout << c2;
+    }
+    else{
+      cout << c1;
+    }
+    cout << endl;
   }
-  else if (players[2][2] == "Hearts"){
-    cout << c2;
-  }
+}
