e8505eb42ee67047cbcd1b54dde13dbe7f436a97
Endolite <41879690+Endolite@users.noreply.github.com>
2022-05-21 01:35:06 -0400

    05/21/22 0135

--- Assignments/Project 1/Project 1 - Arnav Patri.cpp
+++ Assignments/Project 1/Project 1 - Arnav Patri.cpp
@@ -5,49 +5,54 @@
 
 using namespace std;
 int main(){
-  srand(time(0));
+  srand(time(NULL));
   string cards[13] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
   string suites[4] = {"Spades", "Hearts", "Diamonds", "Clubs"};
-  string players[2][3];
-    players[0][0] = "one";
-    players[1][0] = "two";
+  string pn[2] = {"one", "two"};
+  string resp;
+  int players[2][2];
+  do {
     for (int i = 0; i < 2; i++){
-      players[i][1] = cards[rand() % 13];
-      players[i][2] = suites[rand() % 4];
+      players[i][0] = rand() % 13;
+      players[i][1] = rand() % 4;
     }
-    while ((players[0][1] == players[1][1]) && (players[0][2] == players[1][2])){
-      players[1][1] = cards[rand() % 13];
-      players[1][2] = suites[rand() % 4];
+    while ((players[0][0] == players[1][0]) && (players[0][1] == players[1][1])){
+      players[1][0] = rand() % 13;
+      players[1][1] = rand() % 4;
     }
-  for (int i = 0; i < 2; i++){
-    cout << "Player " + players[i][0] + " card is " + players[i][1] + " " +  players[i][2] + "!" << endl;
-  }
-  string c1 = "Player One Wins!";
-  string c2 = "Player Two Wins!";
-  string c3 = "It's a Draw!";
-  if (players[0][2] == "Hearts"){
-    if (players[1][2] == "Hearts"){
-      if (stoi(players[0][1]) < stoi(players[1][1])){
-        cout << c2;
+    for (int i = 0; i < 2; i++){
+      cout << "Player " + pn[i] + " card is " + cards[players[i][0]] + " " +  suites[players[i][1]] + "!" << endl;
+    }
+    string c1 = "Player One Wins!";
+    string c2 = "Player Two Wins!";
+    string c3 = "It's a Draw!";
+    if (players[0][1] == 1){
+      if (players[1][1] == 1){
+        if (players[0][0] < players[1][0]){
+          cout << c2;
+        }
+        else{
+          cout << c1;
+        }
       }
       else{
         cout << c1;
       }
     }
+    else if (players[1][1] == 1){
+      cout << c2;
+    }
+    else if (players[0][0] == players[1][0]){
+      cout << c3;
+    }
+    else if (players[0][0] < players[1][0]){
+      cout << c2;
+    }
     else{
       cout << c1;
     }
-  }
-  else if (players[1][2] == "Hearts"){
-    cout << c2;
-  }
-  else if (stoi(players[0][1]) == stoi(players[1][1])){
-    cout << c3;
-  }
-  else if (stoi(players[0][1]) < stoi(players[1][1])){
-    cout << c2;
-  }
-  else{
-    cout << c1;
-  }
+    cout << '\n' << "Press any key to continue... ";
+    cin.get();
+  } while (cin.get() != '\n');
+  return 0;
 }
