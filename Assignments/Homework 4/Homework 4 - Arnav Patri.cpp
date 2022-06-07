6581d847f8f5ad4c6a4a05383b4b38a84390a0a7
Endolite <41879690+Endolite@users.noreply.github.com>
2022-06-07 08:27:14 -0400

    06/07/22 0827

---
+++ Assignments/Homework 4/Homework 4 - Arnav Patri.cpp
@@ -0,0 +1,43 @@
+// Arnav Patri
+/*I HAVE NEITHER GIVEN NOR RECEIVED UNAUTHORIZED
+AID IN COMPLETING THIS WORK, NOR HAVE I PRESENTED
+SOMEONE ELSE’S WORK AS MY OWN.*/
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b){
+  int temp = a;
+  a = b;
+  b = temp;
+};
+
+void sort(int arr[], int size){
+  bool change = true;;
+  while (change){
+    change = false;
+    for (int i = 0; i < size - 1; i++){
+      if (arr[i] > arr[i + 1]){
+        swap(arr[i], arr[i + 1]);
+        change = true;
+      }
+    }
+  }
+}
+int main(){
+  string str;
+  cout << "Enter the size of your list: ";
+  cin >> str;
+  const int size = stoi(str);
+  int scores[size];
+  for (int i = 0; i < size; i++){
+    cout << "Enter a score: ";
+    cin >> scores[i];
+  }
+  sort(scores, size);
+  cout << endl << "List of scores in ascending order:" << endl;
+  for (int i = 0; i < size; i++){
+    cout << scores[i] << " ";
+  }
+  return 0;
+}
